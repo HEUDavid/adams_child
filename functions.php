1716e62c862642d73846dd186f8cf8711780c6dd
David <david@hrbeu.edu.cn>
2020-05-29 10:18:47 +0800

    O_0

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+if (!function_exists('davidxiang')) {
+    function davidxiang() {
+        // 加载子主题样式
+        echo '<link href="https://mdavid.cn/wp-content/themes/adams_child/style.css" type="text/css" rel="stylesheet">';
+        echo '<script>console.info("无聊的人。。。")</script>' . "\n";
+    }
+}
+
+add_action('wp_head', 'davidxiang');
+
+
+?>
