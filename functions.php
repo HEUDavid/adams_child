4abcb19238d28e8f083f61b780cdae17ce6cc621
David <david@hrbeu.edu.cn>
2020-05-30 01:12:36 +0800

    页面去掉底部导航

--- functions.php
+++ functions.php
@@ -1,13 +1,20 @@
 <?php
-if (!function_exists('davidxiang')) {
-    function davidxiang() {
+if (!function_exists('davidxiang_head')) {
+    function davidxiang_head() {
         // 加载子主题样式
         echo '<link href="https://mdavid.cn/wp-content/themes/adams_child/style.css" type="text/css" rel="stylesheet">';
-        echo '<script>console.info("无聊的人。。。")</script>';
     }
 }
+add_action('wp_head', 'davidxiang_head');
 
-add_action('wp_head', 'davidxiang');
-
+if (!function_exists('davidxiang_footer')) {
+    function davidxiang_footer() {
+        if (is_page()) {
+                echo '<style type="text/css">.nearbypost {display: none;}</style>';
+	}
+	echo '<script>console.info("无聊的人。。。")</script>';
+    }
+}
+add_action('wp_footer', 'davidxiang_footer');
 
 ?>
