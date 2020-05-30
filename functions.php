9d52f17955b50b27a4a7a86642bd11f4a59b7651
David <david@hrbeu.edu.cn>
2020-05-30 16:19:09 +0800

    O_O

--- functions.php
+++ functions.php
@@ -1,19 +1,20 @@
 <?php
+
 if (!function_exists('davidxiang_head')) {
-    function davidxiang_head() {
-        // 加载子主题样式
-        echo '<link href="https://mdavid.cn/wp-content/themes/adams_child/style.css" type="text/css" rel="stylesheet">';
-    }
+	function davidxiang_head() {
+		// 加载子主题样式
+		echo '<link href="https://mdavid.cn/wp-content/themes/adams_child/style.css" type="text/css" rel="stylesheet">';
+	}
 }
 add_action('wp_head', 'davidxiang_head');
 
 if (!function_exists('davidxiang_footer')) {
-    function davidxiang_footer() {
-        if (is_page()) {
-                echo '<style type="text/css">.nearbypost {display: none;}</style>';
+	function davidxiang_footer() {
+		if (is_page()) {
+			echo '<style type="text/css">.nearbypost {display: none;}</style>';
+		}
+		echo '<script>console.info("一个无聊的人，联系我（admin艾特mdavid.cn）。")</script>';
 	}
-	echo '<script>console.info("一个无聊的人，联系我（admin艾特mdavid.cn）。")</script>';
-    }
 }
 add_action('wp_footer', 'davidxiang_footer');
 
